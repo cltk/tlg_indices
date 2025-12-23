3a170795e375453beb57207b3b06d5d178317a76
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2025-12-23 13:01:08 -0800

    add lat phi5 processing for individual texts

--- tlgu_examples.py
+++ tlgu_examples.py
@@ -4,7 +4,7 @@ from tlg_indices.tlgu import tlgu_convert_corpus, tlgu_convert_file
 
 
 def main() -> None:
-    # Convert a single file into author file
+    # Convert a single TLG file into author file
     tlgu_convert_file(
         orig_txt_path="/Users/kylepjohnson/tlg/TLG_E/TLG0007.TXT",
         target_txt_path="/Users/kylepjohnson/Downloads/0007.txt",
@@ -13,32 +13,68 @@ def main() -> None:
         overwrite=True,
     )
 
-    # Convert a single file into works files
-    tlgu_convert_file(
-        orig_txt_path="/Users/kylepjohnson/tlg/TLG_E/TLG0007.TXT",
-        target_txt_path="/Users/kylepjohnson/Downloads/0007",
-        corpus="tlg",
-        grouping="work",
-        overwrite=True,
-    )
+    # # Convert a single TLG file into works files
+    # tlgu_convert_file(
+    #     orig_txt_path="/Users/kylepjohnson/tlg/TLG_E/TLG0007.TXT",
+    #     target_txt_path="/Users/kylepjohnson/Downloads/0007",
+    #     corpus="tlg",
+    #     grouping="work",
+    #     overwrite=True,
+    # )
 
-    # Convert entire corpus into author files
-    tlgu_convert_corpus(
-        orig_txt_dir="/Users/kylepjohnson/tlg/TLG_E",
-        target_txt_dir="/Users/kylepjohnson/Downloads/tlg-authors",
-        corpus="tlg",
+    # # Convert entire TLG corpus into author files
+    # tlgu_convert_corpus(
+    #     orig_txt_dir="/Users/kylepjohnson/tlg/TLG_E",
+    #     target_txt_dir="/Users/kylepjohnson/Downloads/tlg-authors",
+    #     corpus="tlg",
+    #     grouping="author",
+    #     overwrite=True,
+    # )
+
+    # # Convert entire TLG corpus into work files
+    # tlgu_convert_corpus(
+    #     orig_txt_dir="/Users/kylepjohnson/tlg/TLG_E",
+    #     target_txt_dir="/Users/kylepjohnson/Downloads/tlg-works",
+    #     corpus="tlg",
+    #     grouping="work",
+    #     overwrite=True,
+    # )
+
+    # Convert a single PHI5 file into author file
+    tlgu_convert_file(
+        orig_txt_path="/Users/kylepjohnson/tlg/PHI5/LAT1254.TXT",
+        target_txt_path="/Users/kylepjohnson/Downloads/1254.txt",
+        corpus="phi5",
         grouping="author",
         overwrite=True,
     )
 
-    # Convert entire corpus into work files
-    tlgu_convert_corpus(
-        orig_txt_dir="/Users/kylepjohnson/tlg/TLG_E",
-        target_txt_dir="/Users/kylepjohnson/Downloads/tlg-works",
-        corpus="tlg",
+    # Convert a single PHI5 file into works files
+    tlgu_convert_file(
+        orig_txt_path="/Users/kylepjohnson/tlg/PHI5/LAT1254.TXT",
+        target_txt_path="/Users/kylepjohnson/Downloads/1254",
+        corpus="phi5",
         grouping="work",
         overwrite=True,
     )
+
+    # # Convert entire PHI5 corpus into author files
+    # tlgu_convert_corpus(
+    #     orig_txt_dir="/Users/kylepjohnson/phi5/PHI5",
+    #     target_txt_dir="/Users/kylepjohnson/Downloads/phi5-authors",
+    #     corpus="phi5",
+    #     grouping="author",
+    #     overwrite=True,
+    # )
+
+    # # Convert entire PHI5 corpus into work files
+    # tlgu_convert_corpus(
+    #     orig_txt_dir="/Users/kylepjohnson/phi5/PHI5",
+    #     target_txt_dir="/Users/kylepjohnson/Downloads/phi5-works",
+    #     corpus="phi5",
+    #     grouping="work",
+    #     overwrite=True,
+    # )
 
 
 if __name__ == "__main__":
