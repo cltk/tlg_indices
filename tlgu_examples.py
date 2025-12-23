d61329b531fa4257fa279eb22c6b83e30cc511d2
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2025-12-23 13:12:44 -0800

    confirm phi working

--- tlgu_examples.py
+++ tlgu_examples.py
@@ -14,31 +14,31 @@ def main() -> None:
     )
 
     # # Convert a single TLG file into works files
-    # tlgu_convert_file(
-    #     orig_txt_path="/Users/kylepjohnson/tlg/TLG_E/TLG0007.TXT",
-    #     target_txt_path="/Users/kylepjohnson/Downloads/0007",
-    #     corpus="tlg",
-    #     grouping="work",
-    #     overwrite=True,
-    # )
+    tlgu_convert_file(
+        orig_txt_path="/Users/kylepjohnson/tlg/TLG_E/TLG0007.TXT",
+        target_txt_path="/Users/kylepjohnson/Downloads/0007",
+        corpus="tlg",
+        grouping="work",
+        overwrite=True,
+    )
 
     # # Convert entire TLG corpus into author files
-    # tlgu_convert_corpus(
-    #     orig_txt_dir="/Users/kylepjohnson/tlg/TLG_E",
-    #     target_txt_dir="/Users/kylepjohnson/Downloads/tlg-authors",
-    #     corpus="tlg",
-    #     grouping="author",
-    #     overwrite=True,
-    # )
+    tlgu_convert_corpus(
+        orig_txt_dir="/Users/kylepjohnson/tlg/TLG_E",
+        target_txt_dir="/Users/kylepjohnson/Downloads/tlg-authors",
+        corpus="tlg",
+        grouping="author",
+        overwrite=True,
+    )
 
     # # Convert entire TLG corpus into work files
-    # tlgu_convert_corpus(
-    #     orig_txt_dir="/Users/kylepjohnson/tlg/TLG_E",
-    #     target_txt_dir="/Users/kylepjohnson/Downloads/tlg-works",
-    #     corpus="tlg",
-    #     grouping="work",
-    #     overwrite=True,
-    # )
+    tlgu_convert_corpus(
+        orig_txt_dir="/Users/kylepjohnson/tlg/TLG_E",
+        target_txt_dir="/Users/kylepjohnson/Downloads/tlg-works",
+        corpus="tlg",
+        grouping="work",
+        overwrite=True,
+    )
 
     # Convert a single PHI5 file into author file
     tlgu_convert_file(
@@ -67,14 +67,14 @@ def main() -> None:
         overwrite=True,
     )
 
-    # # Convert entire PHI5 corpus into work files
-    # tlgu_convert_corpus(
-    #     orig_txt_dir="/Users/kylepjohnson/tlg/PHI5",
-    #     target_txt_dir="/Users/kylepjohnson/Downloads/phi5-works",
-    #     corpus="phi5",
-    #     grouping="work",
-    #     overwrite=True,
-    # )
+    # Convert entire PHI5 corpus into work files
+    tlgu_convert_corpus(
+        orig_txt_dir="/Users/kylepjohnson/tlg/PHI5",
+        target_txt_dir="/Users/kylepjohnson/Downloads/phi5-works",
+        corpus="phi5",
+        grouping="work",
+        overwrite=True,
+    )
 
 
 if __name__ == "__main__":
